990a302ecf62290fd18b9213f7e075cf013663ec
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-03-14 11:27:48 -0400

    Project Setup. Web server running

---
+++ server.js
@@ -0,0 +1,42 @@
+// Import Modules and set up vars
+require('dotenv').config()
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//connect to database
+const db = require('./models/db')
+db.once('open', () => {
+  console.log('connected to mongo')
+})
+
+//Initialize View Engine
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine())
+
+// Mount Express Middleware
+
+// invoked for any requests passed to this router
+// app.use((req, res, next) => {
+//   res.locals.data = {}
+//   // console.log("server js " + res.locals.data)
+//   next()
+// }) // Creates res.locals.data
+
+app.use(express.urlencoded({ extended: true })) // Creates req.body
+app.use(methodOverride('_method')); // Allows us to override methods
+app.use(express.static('public')); // Allows us to have Static Files
+
+// Mounts our RESTFUL/INDUCES ROUTES at /fruits
+// we can use this router inside of our server.js as Middleware
+// app.use('/fruits', require('./controllers/routeController.js')); 
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+// Listen on PORT
+app.listen(PORT, () => {
+  console.log('Listening on port ', PORT)
+})
